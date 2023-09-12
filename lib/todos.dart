d1d078ad757c30f80c666365603aebe73760a8ad
Jaycee Buarao <jayceebuarao@gmail.com>
2023-09-11 17:43:00 -0700

    added open modal and add new to do item method

--- lib/todos.dart
+++ lib/todos.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/widgets/models/todo.dart';
+import 'package:todo_list/widgets/newtodo.dart';
+import 'package:todo_list/widgets/todos_list/todo_list.dart';
 
 class ToDos extends StatefulWidget {
   const ToDos({super.key});
@@ -8,21 +11,34 @@ class ToDos extends StatefulWidget {
 }
 
 class _ToDosState extends State<ToDos> {
+  final List<ToDo> _registeredToDos = [ToDo(task: 'Your Mom', isDone: false)];
+
+  void addNewToDo(ToDo newTodo) {
+    setState(() {
+      _registeredToDos.add(newTodo);
+    });
+  }
+
+  void openNewToDoModal() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => NewTodo(updateToDo: addNewToDo));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('To Do List'),
       ),
-      body: const Column(
+      body: Column(
         children: [
-          Text('List Items Goes Here'),
-          Text('Hello world'),
+          Expanded(child: ToDoList(todos: _registeredToDos)),
         ],
       ),
       floatingActionButton: FloatingActionButton.large(
+        onPressed: openNewToDoModal,
         child: const Icon(Icons.add),
-        onPressed: () {},
       ),
     );
   }
